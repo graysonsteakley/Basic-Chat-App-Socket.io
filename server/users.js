2f70d0420843ba5203ec3fee7bbe8279b6ed01e6
Gray Steakley <grayson.steakley@gmail.com>
2020-07-02 15:19:13 -0500

    fixed styling

--- server/users.js
+++ server/users.js
@@ -1,42 +1,29 @@
-// HELPER functions to keep track of users
-
-
 const users = [];
 
-const addUser = ({id, name, room})=>{
+const addUser = ({ id, name, room }) => {
+  name = name.trim().toLowerCase();
+  room = room.trim().toLowerCase();
 
-    //Javascript Mastery = javascriptmastery
+  const existingUser = users.find((user) => user.room === room && user.name === name);
 
-    name = name.trim().toLowerCase();
-    room = room.trim().toLowerCase();
+  if(!name || !room) return { error: 'Username and room are required.' };
+  if(existingUser) return { error: 'Username is taken.' };
 
-    // check for username already in the room
-    const existingUser = users.find(user=>user.room === room && user.name === name);
+  const user = { id, name, room };
 
-    if (existingUser){
-        return {err: 'username is taken.'}
-    }
+  users.push(user);
 
-    const user = {id, name, room};
-
-    users.push(user);
-
-    return {user};
-
+  return { user };
 }
 
-const removeUser = (id)=>{
+const removeUser = (id) => {
+  const index = users.findIndex((user) => user.id === id);
 
-    const ind = users.findIndex(user=> user.id === id);
-
-    if(index !== -1){
-        return users.splice(index, 1)[0];
-    }
-
+  if(index !== -1) return users.splice(index, 1)[0];
 }
 
-const getUser = (id)=> users.find(user=> user.id === id);
+const getUser = (id) => users.find((user) => user.id === id);
 
-const getUsersinRoom = (room) => users.filter(user=> user.room === room);
+const getUsersInRoom = (room) => users.filter((user) => user.room === room);
 
-module.exports = { addUser, removeUser, getUser, getUsersinRoom};
+module.exports = { addUser, removeUser, getUser, getUsersInRoom };
